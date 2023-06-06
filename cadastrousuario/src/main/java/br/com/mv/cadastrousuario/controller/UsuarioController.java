5e46e1b02be1ce565f6ad89f6080d8c939f1dd08
marcelo.cavalcanti <marcelodemoura14@gmail.com>
2023-06-06 11:20:26 -0300

    update

--- cadastrousuario/src/main/java/br/com/mv/cadastrousuario/controller/UsuarioController.java
+++ cadastrousuario/src/main/java/br/com/mv/cadastrousuario/controller/UsuarioController.java
@@ -1,26 +1,53 @@
 package br.com.mv.cadastrousuario.controller;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import br.com.mv.cadastrousuario.dto.UsuarioDto;
 import br.com.mv.cadastrousuario.service.UsuarioServiceImpl;
 import lombok.RequiredArgsConstructor;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/usuario")
 @RequiredArgsConstructor
 public class UsuarioController {
     
-    private UsuarioServiceImpl service;
+    private final UsuarioServiceImpl service;
 
-    @PostMapping
+    @PostMapping()
     @ResponseStatus(HttpStatus.CREATED)
     public UsuarioDto salvar(@RequestBody UsuarioDto dto){
         return service.save(dto);
+    }
+
+    @GetMapping()
+    @ResponseStatus(HttpStatus.OK)
+    public Page<UsuarioDto> listarTodos(@PageableDefault(page = 0, size = 10, sort = "id")Pageable pageable){
+        return service.findAll(pageable);
+    }
+    @GetMapping("/id")
+    @ResponseStatus(HttpStatus.OK)
+    public Optional<UsuarioDto> buscarId(@PathVariable Long id){
+        Optional <UsuarioDto>dto = service.findById(id);
+        return dto;
+    }
+    @PatchMapping("/id")
+    @ResponseStatus(HttpStatus.OK)
+    public UsuarioDto atualizar(@PathVariable Long id,@RequestBody UsuarioDto dto){
+        dto = service.update(id, dto);
+        return dto;
+    }
+
+    @DeleteMapping("id")
+    @ResponseStatus(HttpStatus.OK)
+    public UsuarioDto apagar(@PathVariable Long id){
+        service.delete(id);
+        return null;
+
     }
 }
--- cadastrousuario/src/main/java/br/com/mv/cadastrousuario/service/UsuarioServiceImpl.java
+++ cadastrousuario/src/main/java/br/com/mv/cadastrousuario/service/UsuarioServiceImpl.java
@@ -1,27 +1,79 @@
 package br.com.mv.cadastrousuario.service;
-import java.time.LocalDateTime;
-
-import org.springframework.beans.BeanUtils;
-import org.springframework.stereotype.Service;
 
 import br.com.mv.cadastrousuario.dto.UsuarioDto;
 import br.com.mv.cadastrousuario.entity.Usuario;
 import br.com.mv.cadastrousuario.repository.UsuarioRepository;
+import br.com.mv.cadastrousuario.service.exception.BadRequestException;
+import br.com.mv.cadastrousuario.service.exception.NotFoundException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.BeanUtils;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
-public class UsuarioServiceImpl implements UsuarioService{
-
-    private UsuarioRepository repository;
+public class UsuarioServiceImpl implements UsuarioService {
+    private final UsuarioRepository repository;
 
     @Override
     public UsuarioDto save(UsuarioDto dto) {
         Usuario entity = new Usuario();
         BeanUtils.copyProperties(dto, entity);
+        this.validationCpf(entity);
         entity.setDataCriacao(LocalDateTime.now());
         repository.save(entity);
         return dto;
     }
-    
+
+    @Override
+    public Page<UsuarioDto> findAll(Pageable pageable) {
+        Page<Usuario>list =repository.findAll(pageable);
+        return list.map(x -> new UsuarioDto(x));
+    }
+
+    @Override
+    public Optional<UsuarioDto> findById(Long id){
+        Optional<Usuario>usuarioOptional = repository.findById(id);
+        Usuario entity = usuarioOptional.orElseThrow(() -> new NotFoundException("Usuario não encontrado"));
+        return Optional.of(new UsuarioDto(entity));
+    }
+
+    @Override
+    public UsuarioDto update(Long id, UsuarioDto dto) {
+        Usuario entity = repository.findById(id).orElseThrow(()-> new NotFoundException("Usuario não encontrado"));
+        if (dto.getCpf()!=null){
+            entity.setCpf(dto.getCpf());
+        }
+        if (dto.getNome() != null){
+            entity.setNome(dto.getNome());
+        }
+        if (dto.getEmail() != null){
+            entity.setEmail(dto.getEmail());
+        }
+        if (dto.getTelefone() != null){
+            entity.setTelefone(dto.getTelefone());
+        }
+        entity.setDataAtualizacao(LocalDateTime.now());
+        repository.save(entity);
+        return new UsuarioDto(entity);
+    }
+
+    @Override
+    public void delete(Long id) {
+        repository.findById(id).orElseThrow(()-> new NotFoundException("Usuario de" +id + "não foi encontrado."));
+        repository.deleteById(id);
+
+    }
+    private void validationCpf(Usuario entity){
+        Optional<Usuario>usuarioOptional = repository.findByCpf(entity.getCpf());
+        if (usuarioOptional.isPresent()){
+            throw new BadRequestException("Cpf já cadastrado!");
+        }
+
+    }
+
 }
